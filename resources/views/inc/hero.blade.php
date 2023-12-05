15b0ef8eccf85d4fce2c567b21a46018626c6c9d
MrF0o <fathihelmi@outlook.com>
2023-12-05 23:21:36 +0100

    created home page

---
+++ resources/views/inc/hero.blade.php
@@ -0,0 +1,15 @@
+<div class="container max-w-lg px-4 py-44 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
+    style="background-image: url('https://cdn.pixabay.com/photo/2017/06/06/22/46/mediterranean-cuisine-2378758_1280.jpg')">
+    <h1
+        class="md:text-5xl text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-center sm:leading-none lg:text-5xl">
+        <span class="inline md:block">Welcome To Larainfo Restaurant</span>
+    </h1>
+    <div class="mx-auto mt-2 text-green-50 md:text-center lg:text-xl font-medium">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br> Soluta voluptatem ab necessitatibus illo praesentium
+        culpa excepturi quae commodi quaerat,
+    </div>
+    <div class="flex flex-col items-center mt-12 text-center">
+        <span class="relative inline-flex w-full md:w-auto">
+            <button class="inline-flex items-center text-xl justify-center px-5 py-4 font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 transition-all duration-300 focus:outline-none">Reserve Now</button>
+    </div>
+</div>
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Resto</title>
+
+    @vite('resources/css/app.css')
+    <script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
+</head>
+<body>
+    
+    @include('inc.header')
+
+    @yield('content')
+
+    @include('inc.footer')
+
+</body>
+</html>
