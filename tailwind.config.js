2deed4effa3a41924a6c42b23ce33e0326d2b2da
MrF0o <fathihelmi@outlook.com>
2023-12-05 23:55:06 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const colors = require('tailwindcss/colors')
 
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+}
